c6e7eb1c60181e1ade3a7ffb83c748ae1192f2f3
rahul-acr <rahul.saha.c@gmail.com>
2020-07-18 14:56:33 +0530

    Minor refactoring

--- lh-bootstrap/src/main/java/org/social/it/DatabaseConfiguration.java
+++ lh-bootstrap/src/main/java/org/social/it/DatabaseConfiguration.java
@@ -1,28 +1,15 @@
 package org.social.it;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
 @EnableMongoRepositories(basePackages = "org.social.it.**.repository")
 @Configuration
 public class DatabaseConfiguration {
-
-//    @Bean
-//    public MongoClient mongoClient() {
-//        return MongoClients.create("mongodb://localhost:27017");
-//    }
-//
-//    @Bean
-//    public MongoTemplate mongoTemplate() {
-//        return new MongoTemplate(mongoClient(), "test");
-//    }
 
     @Bean
     public MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
