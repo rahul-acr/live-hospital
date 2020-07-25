a5ebb76b32cf9613201d670f8231a7f873cba77a
rahul-acr <rahul.saha.c@gmail.com>
2020-07-25 22:16:29 +0530

    Word distance calculators added

---
+++ lh-extraction/src/main/java/com/social/it/impl/LevenshteinDistanceCalculator.java
@@ -0,0 +1,32 @@
+package com.social.it.impl;
+
+import com.social.it.WordDistanceCalculator;
+
+
+public class LevenshteinDistanceCalculator implements WordDistanceCalculator {
+
+    private static int min(int a, int b, int c) {
+        return Math.min(a, Math.min(b, c));
+    }
+
+    @Override
+    public int calculateDistance(CharSequence input1, CharSequence input2) {
+        int[][] dp = new int[input1.length() + 1][input2.length() + 1];
+
+        for (int i = 0; i <= input1.length(); i++) {
+            for (int j = 0; j <= input2.length(); j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                } else if (j == 0) {
+                    dp[i][j] = i;
+                } else {
+                    dp[i][j] = min(dp[i - 1][j - 1] + (input1.charAt(i - 1) == input2.charAt(j - 1) ? 0 : 1),
+                            dp[i - 1][j] + 1,
+                            dp[i][j - 1] + 1);
+                }
+            }
+        }
+
+        return dp[input1.length()][input2.length()];
+    }
+}
